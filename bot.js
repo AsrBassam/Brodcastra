02973d15d7183afe481fef68660fde23f1f8a898
AsrBassam <45674276+AsrBassam@users.noreply.github.com>
2019-01-18 07:49:40 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on("ready", async() => {
     client.user.setGame("Red-Army");
 console.log(`Back Online In ${client.guilds.size} Servers!`);  
     setTimeout(() => {
-        client.user.setActivity(`${prefix}help | V 1.1`, {type: "WATCHING"});
+        client.user.setActivity(`${prefix}help | Red Army`, {type: "WATCHING"});
     }, 3000);
 });
 
